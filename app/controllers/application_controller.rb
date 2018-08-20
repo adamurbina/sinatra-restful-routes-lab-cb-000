787cee130c5e4636e369ed8c61a7e9fed5c6d2e0
Adam Urbina <adamurbina@gmail.com>
2018-08-20 18:09:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,11 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  post '/recipes' do
+    @recipe = Recipe.create()
+    redirect "/recipes/#{@recipe.id}"
+  end
+
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :edit
